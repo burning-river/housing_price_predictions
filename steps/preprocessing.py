59f290ac62616e2bf98841842bbc55c89fb26234
Saurabh Kumar <saurabhkumar3400@gmail.com>
2024-11-25 16:34:29 -0500

    scaling, normalization added to preprocessing

--- steps/preprocessing.py
+++ steps/preprocessing.py
@@ -4,7 +4,7 @@ import numpy as np
 from zenml import step
 from src.data_preprocessing import DataPreProcessing, DataCleaningStrategy, \
 ImputingMissingValues, DroppingRedundantColumns, RemovingOutliers, \
-OneHotEncoding, ExtractingTarget
+OneHotEncoding, ExtractingTarget, FeatureSelection, FeatureScaling
 from typing_extensions import Annotated
 from typing import Tuple 
 
@@ -17,17 +17,17 @@ def preprocess_df(df: pd.DataFrame) -> Tuple[
     # print('Original shape: ', df.shape)
     clean_strategy = DataCleaningStrategy()
     data_cleaning = DataPreProcessing(df, clean_strategy)
-    processed_data, dropped_columns = data_cleaning.handle_data()
+    processed_data, _ = data_cleaning.handle_data()
     # print('shape after dropping Nan columns: ', processed_data.shape)
 
     impute_strategy = ImputingMissingValues()
     data_imputing = DataPreProcessing(processed_data, impute_strategy)
-    processed_data, cols_imputed_vals_dict = data_imputing.handle_data()  
+    processed_data, _ = data_imputing.handle_data()  
     # print('shape after imputing missing vals: ', processed_data.shape)
 
     drop_redundancy_strategy = DroppingRedundantColumns()
     data_redundancy = DataPreProcessing(processed_data, drop_redundancy_strategy)
-    processed_data, redundant_columns = data_redundancy.handle_data() 
+    processed_data, _ = data_redundancy.handle_data() 
     # print('shape after dropping redundant columns: ', processed_data.shape)
 
     drop_outliers_strategy = RemovingOutliers()
@@ -42,8 +42,17 @@ def preprocess_df(df: pd.DataFrame) -> Tuple[
 
     ohe_strategy = OneHotEncoding()
     data_ohe = DataPreProcessing(processed_data, ohe_strategy)
-    processed_data, ohe_transformer, ordinal_variables, feat_names = data_ohe.handle_data() 
+    processed_data, _, _, _ = data_ohe.handle_data() 
     # print('shape after OHE: ', processed_data.shape)
+
+    feature_selection_strategy = FeatureSelection()
+    data_uncorrelated = DataPreProcessing(processed_data, feature_selection_strategy)
+    processed_data, _ = data_uncorrelated.handle_data()
+    # logging.info(f'Shape of pruned dataset: {X_train.shape}')
+
+    feature_scaling_strategy = FeatureScaling()
+    data_scaled = DataPreProcessing(processed_data, feature_scaling_strategy)
+    processed_data, _ = data_scaled.handle_data()
 
     # print('final shape of training data: ', processed_data.shape)
 
